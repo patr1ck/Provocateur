97d05e2ce420133d6b593fe9d0a63c9bac456ec2
Patrick B. Gibson <patrick@fadeover.org>
2014-04-14 16:33:08 -0700

    Add podspec

--- ProvocateurExample/ProvocateurExample/PAHAppDelegate.m
+++ ProvocateurExample/ProvocateurExample/PAHAppDelegate.m
@@ -14,7 +14,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
     [[Provocateur sharedInstance] listen];
     
     return YES;
